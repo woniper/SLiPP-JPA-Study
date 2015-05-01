702a3d9addfcb69b0cf47187f7262b82e0161607
woniper <leekw3747@gmail.com>
2015-05-02 05:01:43 +0900

    JPA javaConfig 설정 - LocalEntityManagerFactoryBean - JpaVenderAdapter

---
+++ SpringBoot-JPA/src/main/java/net/woniper/jpa/config/JPAConfig.java
@@ -0,0 +1,60 @@
+package net.woniper.jpa.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import java.util.Properties;
+
+/**
+ * Created by woniper on 15. 5. 2..
+ */
+@Configuration
+@EnableTransactionManagement
+public class JPAConfig {
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean() {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource());
+        em.setPackagesToScan("net.woniper.jpa.domain");
+
+        // persistence 설정
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.connection.url", "jdbc:h2:file:./data/jpa;DB_CLOSE_ON_EXIT=FALSE");
+        properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+        properties.setProperty("hibernate.show_sql", "true");
+
+        // 각 구현체의 프로퍼티 확장 및 설정
+        JpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(jpaVendorAdapter);
+        em.setJpaProperties(properties);
+        return em;
+    }
+
+    // H2 DB 설정
+    @Bean(destroyMethod = "shutdown")
+    public EmbeddedDatabase dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .setName("slipp")
+                .build();
+    }
+
+    // Transaction 설정
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(emf);
+        return transactionManager;
+    }
+}
---
+++ SpringBoot-JPA/src/main/java/net/woniper/jpa/domain/User.java
@@ -0,0 +1,84 @@
+package net.woniper.jpa.domain;
+
+import javax.persistence.*;
+import java.util.List;
+
+/**
+ * Created by woniper on 15. 4. 30..
+ */
+@Entity(name = "tbl_user")
+public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer userId;
+
+    private String username;
+
+    private String nickName;
+
+    private String address;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Order> orders;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+
+    public int totalPrice() {
+        int totalPrice = 0;
+
+        for (Order order : orders) {
+            totalPrice += order.getPrice();
+        }
+
+        return totalPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", username='" + username + '\'' +
+                ", nickName='" + nickName + '\'' +
+                ", address='" + address + '\'' +
+                ", orders=" + orders +
+                '}';
+    }
+}
